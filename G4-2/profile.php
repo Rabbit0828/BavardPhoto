e609aee82091901e7597fa7a50f6519f06d0f416
atomoa <tomo92588481129@icloud.com>
2024-05-15 12:37:54 +0900

    5.15日の成果

--- G4-2/profile.php
+++ G4-2/profile.php
@@ -1,6 +1,23 @@
-<?php require ./header.php ?>
-<?php require dbconnect.php ?>
-
-
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>BavardPhotos</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php require 'dbconnect.php'; ?> 
+    <?php 
+    $pdo = new PDO($connect, user, pass);
+    $sql = $pdo->prepare('select * from UserTable where user_id = ?');
+    $sql->execute([$_GET['id']]);
+    foreach ($sql as $row) {
+        echo '<div class="user-name">', $row['user_name'],'</div>';
+        echo '<div class="profile-image"><img src="', $row['icon'], '"></div>';
+        
+        echo '<div class="follow"><a href=followyou.php>フォロー</a></div>';
+        echo '<div class="message"><a href=message.php>メッセージ</a></div>';
+        echo '<div class="private-name">', $row['private_name'],'</div>';
+        echo '<div class="vio">', $row['private_name'],'</div>';
 </body>
 </html>

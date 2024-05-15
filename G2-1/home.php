16c9accc0cf4c50fcb6f9a47e8f045956abe1beb
2201236 <2201236@s.asojuku.ac.jp>
2024-05-15 10:36:56 +0900

    h

---
+++ G2-1/home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Hamburger Menu</title>
+<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<div class="menu-toggle">
+  <input type="checkbox" id="menu-toggle-checkbox" />
+  <label for="menu-toggle-checkbox" class="menu-toggle-button">
+    <span></span>
+    <span></span>
+    <span></span>
+  </label>
+  <ul class="menu">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Post</a></li>
+    <li><a href="#">Story</a></li>
+    <li><a href="#">Toukou</a></li>
+    <li><a href="#">My Page</a></li>
+    <li><a href="#">Option</a></li>
+    <li><a href="#">Logout</a></li>
+  </ul>
+</div>
+</body>
+</html>

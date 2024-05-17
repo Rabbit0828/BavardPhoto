2803ccd54f0825cdf6f1e238868ec5e10515e218
oonishi0321 <146159310+oonishi0321@users.noreply.github.com>
2024-05-17 12:39:05 +0900

    a

--- G6-2/G6-2.php
+++ G6-2/G6-2.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>パスワード再設定</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="G6-2.css">
 </head>
 <body>
     <h1>パスワード変更</h1>

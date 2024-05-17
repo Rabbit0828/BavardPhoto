aa1c1e56ba7182a62b196da699f67f4f141b4e72
atomoa <tomo92588481129@icloud.com>
2024-05-17 13:45:43 +0900

    a

--- G4-2/dbconnect.php
+++ G4-2/dbconnect.php
@@ -4,6 +4,7 @@
      const USER ='LAA1517469';
      const PASS ='Pass1234';
     $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
-    $pdo=new PDO('mysql:host=mysql304.phy.lolipop.lan;dbname=LAA1517469-sistem;charset=utf8','LAA1517469','Pass1234');
+    
+    $pdo=new PDO('mysql:host=mysql304.phy.lolipop.lan;dbname=LAA1517469-photos;charset=utf8','LAA1517469','Pass1234');
 
 ?>

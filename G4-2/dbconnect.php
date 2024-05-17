58b3469e40ca274bf4dfd1acfac5d61d5854e514
atomoa <tomo92588481129@icloud.com>
2024-05-17 13:39:38 +0900

    a

--- G4-2/dbconnect.php
+++ G4-2/dbconnect.php
@@ -1,6 +1,6 @@
 <?php
      const SERVER = 'mysql304.phy.lolipop.lan';
-     const DBNAME = 'LAA1517469-sistem';
+     const DBNAME = 'LAA1517469-photos';
      const USER ='LAA1517469';
      const PASS ='Pass1234';
     $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';

680ec475e00116ccea6a1a6c0bacb7c6ec656238
fujirai <2201188@s.asojuku.ac.jp>
2024-05-23 11:37:36 +0900

    h

--- G4-2/follow.php
+++ G4-2/follow.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="UTF-8">
         <title>フォロー画面</title>
-        <link rel="stylesheet" href="css/edit.css">
+        <link rel="stylesheet" href="css/follow.css">
     </head>
     <body>
     <div class="header">
--- Kadai04DbAop/src/main/java/com/example/demo/repository/FoodsCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.example.demo.entity.Foods;
-public interface FoodsCrudRepository extends CrudRepository<Foods, Integer> {
-
-}

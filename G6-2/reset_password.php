3d36c1b84b47ad2a45b783d0d495e481a82ef497
oonishi0321 <146159310+oonishi0321@users.noreply.github.com>
2024-05-24 12:20:34 +0900

    a

--- G6-2/reset_password.php
+++ G6-2/reset_password.php
@@ -13,12 +13,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         $conn = new mysqli($servername, $username, $password, $dbname);
 
-        // 接続確認
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
 
-        // パスワード更新処理（仮のユーザーIDとして1を使用）
         $sql = "UPDATE users SET password='$hashedPassword' WHERE id=1";
 
         if ($conn->query($sql) === TRUE) {
--- G6-2/search_account.php
+++ G6-2/search_account.php
@@ -9,7 +9,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // 接続確認
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }

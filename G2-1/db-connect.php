d058a2864ca5e708b791977b302d392267ff4a19
2201236 <2201236@s.asojuku.ac.jp>
2024-05-30 12:19:05 +0900

    a

---
+++ G2-1/db-connect.php
@@ -0,0 +1,9 @@
+<?php
+     const SERVER = 'mysql304.phy.lolipop.lan';
+     const DBNAME = 'LAA1517469-photos';
+     const USER ='LAA1517469';
+     const PASS ='Pass1234';
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+    $pdo=new PDO($connect,USER,PASS);
+ 
+?>

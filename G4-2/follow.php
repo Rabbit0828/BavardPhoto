1b0c8b516278b202c7b3d435ee5d7312a27c037c
fujikawaaaaa <2201188@s.asojuku.ac.jp>
2024-05-30 12:21:03 +0900

    Update follow.php

--- G4-2/follow.php
+++ G4-2/follow.php
@@ -11,28 +11,31 @@
         <?php  
             $pdo=new PDO($connect,USER,PASS);  
             foreach($pdo->query('select * from FollowRelationship') as $row){
-                $follower_count_sql = 'SELECT COUNT(*) FROM FollowRelationship WHERE user_id = :user_id';
-                $follower_count_stmt = $pdo->prepare($follower_count_sql);
-                $user_id=$row['user_id'];
-                $follower_count_stmt->execute(['user_id' => $user_id]);
-                $follower_count = $follower_count_stmt->fetchColumn();
             echo '<div class="stats">';
+                $follower_count_sql = 'SELECT COUNT(*) FROM FollowRelationship WHERE follow_id = :follow_id';
+                $follower_count_stmt = $pdo->prepare($follower_count_sql);
+                $follower_id=$row['follow_id'];
+                $follower_count_stmt->execute(['follow_id' => $follower_id]);
+                $follower_count = $follower_count_stmt->fetchColumn();
             echo '<div class="followers">',$follower_count,'フォロワー</div>';
-            echo '<div class="following">フォロー中</div>';
+            $user_count_sql = 'SELECT COUNT(*) FROM FollowRelationship WHERE user_id = :user_id';
+            $user_count_stmt = $pdo->prepare($user_count_sql);
+            $user_id=$row['user_id'];
+            $user_count_stmt->execute(['user_id' => $user_id]);
+            $user_count = $user_count_stmt->fetchColumn();
+            echo '<div class="following">',$user_count,'フォロー中</div>';
         }
         ?>
 
-     <input id="box2" name="s" type="text" placeholder="キーワードを入力" />
-        <button id="btn2" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
-    </form>
+   
     <div class="list">
-    <div class="list-item">
-    <div class="profile-info">
-    <div class="name">aiueo</div>
-    <div class="details">あいうえお</div>
-    </div>
-    <div class="follow-button">フォロー中</div>
-    </div>
+        <div class="list-item">
+            <div class="profile-info">
+                <div class="name">aiueo</div>
+                <div class="details">あいうえお</div>
+            </div>
+        <div class="follow-button">フォロー中</div>
+        </div>
     <!-- Repeat the .list-item block as needed -->
     </div>
  

e05da7242324125b422a67af4d957050ce2d79ef
atomoa <tomo92588481129@icloud.com>
2024-06-12 10:38:57 +0900

    G_G

--- G4-1/profile.php
+++ G4-1/profile.php
@@ -28,7 +28,7 @@
 
             echo '<div class="profile_name">', htmlspecialchars($user['user_name'] ?? ''), '</div>';
             echo '<div class="profile_head_text">';
-            echo '<div class="profile_head_icon"><span><img src="image/', htmlspecialchars($user['icon'] ?? ''), '"></span></div>';
+            echo '<div class="profile_head_icon"><span><img src="../images/', htmlspecialchars($user['icon'] ?? ''), '"></span></div>';
             echo '<div class="profile_head_count">';
             echo '<span>投稿</span>';
             echo htmlspecialchars($post_count); //投稿数
@@ -76,7 +76,7 @@
                 echo '<div class="post">';
                 foreach ($posts as $post) {
                     echo '<a href="#" class="post-link" data-image-id="', htmlspecialchars($post['image_id'] ?? ''), '">';
-                    echo '<img src="', htmlspecialchars($post['image_name'] ?? ''), '">';
+                    echo '<img src="../images/', htmlspecialchars($post['image_name'] ?? ''), '">';
                     echo '</a>';
                 }
                 echo '</div>';

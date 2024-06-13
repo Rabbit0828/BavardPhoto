23a8b5ae147b604d6e4c0ecf74a45f6643d50333
atomoa <tomo92588481129@icloud.com>
2024-06-13 11:47:15 +0900

    a

--- G4-1/profile.php
+++ G4-1/profile.php
@@ -35,11 +35,11 @@
             echo '</div>';
             echo '<div class="profile_head_count">';
             echo '<span>フォロワー</span>';
-            echo '<a href="ff.php?user_id=', $user_id, '&type=followers">',htmlspecialchars($follower_count),'</a>'; //フォロワー数
+            echo '<a href="ff.php?user_id=', $user_id, '&type=following">',htmlspecialchars($follower_count),'</a>'; //フォロワー数
             echo '</div>';
             echo '<div class="profile_head_count">';
             echo '<span>フォロー中</span>';
-            echo '<a href="ff.php?user_id=', $user_id, '&type=following">',htmlspecialchars($following_count),'</a>'; //フォロー数
+            echo '<a href="ff.php?user_id=', $user_id, '&type=followers">',htmlspecialchars($following_count),'</a>'; //フォロー数
             echo '</div>';
             echo '<div class="private-name">', htmlspecialchars($user['private_name'] ?? ''), '</div>';
             echo '<div class="profile_actions">';

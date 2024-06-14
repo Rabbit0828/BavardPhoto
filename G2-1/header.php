923001a0ccb9cb27c76e58b284db2403493764bf
2201236 <2201236@s.asojuku.ac.jp>
2024-06-14 11:55:27 +0900

    a

--- G2-1/header.php
+++ G2-1/header.php
@@ -100,7 +100,7 @@
 
     <!-- 位置情報に枠線を追加 -->
     <div class="icon icon2">
-      <a href="favorite.php" target="_self">
+      <a href="https://ja.wikipedia.org/wiki/%E9%98%BF%E9%83%A8%E5%AF%9B" target="_self">
         <img src="../images/gps.png" alt="お気に入り" style="height: 70px;width:75px;" border="0">
       </a>
     </div>

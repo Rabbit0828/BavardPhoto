8ef764ff3df726354f45c767645fe7d8412c08f5
2201236 <2201236@s.asojuku.ac.jp>
2024-06-19 12:11:28 +0900

    a

--- G2-1/header.php
+++ G2-1/header.php
@@ -3,7 +3,6 @@
 <head>
 <meta charset="utf-8">
 <title>CSS入門-ヘッダーとフッターの固定表示</title>
-<link rel="stylesheet" href="styles.css">
 <style>
   .Header {
     display: flex;
@@ -14,12 +13,14 @@
     border-bottom: 1px solid #ddd;
     height: 100px;
     position: relative;
+    z-index: 1000; /* ヘッダーを前面に表示 */
   }
-  .icon-container, .icon-back {
+  .icon-container,.menu-toggle {
     display: flex;
     gap: 10px;
+    margin-right: 40px; /* 左に20px移動 */
   }
-  .icon img, .icon-back img {
+  .icon img {
     height: 70px;
     width: 75px;
   }
@@ -34,30 +35,63 @@
     margin: 0;
   }
   .menu-toggle {
-    position: absolute;
-    top: 10px;
-    right: 20px;
+    display: flex;
+    align-items: center;
+    margin-left: -20px; /* 左に20px移動 */
   }
   .menu-toggle-button {
     display: flex;
     flex-direction: column;
-    gap: 5px;
+    justify-content: space-between;
+    width: 30px;
+    height: 24px;
+    cursor: pointer;
+    z-index: 1001; /* メニューアイコンを前面に表示 */
   }
   .menu-toggle-button span {
     display: block;
-    width: 25px;
-    height: 3px;
+    width: 100%;
+    height: 4px;
     background-color: #DC34E0; /* 三本線の色を指定 */
+    transition: transform 0.3s, opacity 0.3s;
+  }
+  #menu-toggle-checkbox {
+    display: none; /* チェックボックスを非表示にする */
+  }
+  #menu-toggle-checkbox:checked + .menu-toggle-button span:nth-child(1) {
+    transform: rotate(45deg) translateY(10px);
+  }
+  #menu-toggle-checkbox:checked + .menu-toggle-button span:nth-child(2) {
+    opacity: 0;
+  }
+  #menu-toggle-checkbox:checked + .menu-toggle-button span:nth-child(3) {
+    transform: rotate(-45deg) translateY(-10px);
   }
   .menu {
     display: none;
+    position: absolute;
+    top: 100%; /* ヘッダーの下に表示 */
+    right: 0;
+    background-color: white;
+    border: 1px solid #ddd;
+    box-shadow: 0 2px 10px rgba(0,0,0,0.1);
+    z-index: 999; /* メニューを前面に表示 */
+    width: 200px; /* メニューの幅を指定 */
+  }
+  .menu li {
+    list-style: none;
+  }
+  .menu li a {
+    display: block;
+    padding: 10px 20px;
+    color: black;
+    text-decoration: none;
+  }
+  .menu li a:hover {
+    background-color: #f0f0f0;
   }
   #menu-toggle-checkbox:checked + .menu-toggle-button + .menu {
     display: block;
-  }
-  .icon-container {
-    position: relative;
-    left: -100px; /* 左に100px移動 */
   }
   .search-container {
     display: flex;
@@ -72,6 +106,35 @@
     font-size: 16px; /* フォントサイズを指定 */
     border: 2px solid #DC34E0; /* 枠線の色と太さを指定 */
     color: #DC34E0; /* 文字の色を指定 */
+  }
+  @media screen and (max-width: 768px) {
+    .Header {
+      flex-wrap: wrap;
+      justify-content: center;
+      height: auto;
+    }
+    .logo {
+      order: 1;
+      width: 100%;
+      text-align: center;
+      margin-bottom: 10px;
+    }
+    .search-container {
+      order: 2;
+      width: 100%;
+      margin-bottom: 10px;
+    }
+    .icon-container {
+      order: 3;
+      justify-content: center;
+      width: 100%;
+      margin-bottom: 10px;
+    }
+    .menu-toggle {
+      order: 4;
+      width: 100%;
+      text-align: center;
+    }
   }
 </style>
 </head>

666fb2c226419926d7ebf709f7dba83cec1744af
fujikawaaaaa <2201188@s.asojuku.ac.jp>
2024-06-20 12:46:56 +0900

    a

--- HeaderFile/header_mypage.php
+++ HeaderFile/header_mypage.php
@@ -3,9 +3,8 @@
 <head>
 <meta charset="utf-8">
 <title>CSS入門-ヘッダーとフッターの固定表示</title>
-<link rel="stylesheet" href="styles.css">
 <style>
-  .Header {
+   .Header {
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -14,12 +13,14 @@
     border-bottom: 1px solid #ddd;
     height: 100px;
     position: relative;
+    z-index: 1000; /* ヘッダーを前面に表示 */
   }
-  .icon-container, .icon-back {
+  .icon-container,.menu-toggle {
     display: flex;
     gap: 10px;
+    margin-right: 40px; /* 左に20px移動 */
   }
-  .icon img, .icon-back img {
+  .icon img {
     height: 70px;
     width: 75px;
   }
@@ -34,30 +35,69 @@
     margin: 0;
   }
   .menu-toggle {
-    position: absolute;
-    top: 10px;
-    right: 20px;
+    display: flex;
+    align-items: center;
+    margin-left: -20px; /* 左に20px移動 */
   }
   .menu-toggle-button {
     display: flex;
     flex-direction: column;
-    gap: 5px;
+    justify-content: space-between;
+    width: 30px;
+    height: 24px;
+    cursor: pointer;
+    z-index: 1001; /* メニューアイコンを前面に表示 */
   }
   .menu-toggle-button span {
     display: block;
-    width: 25px;
-    height: 3px;
-    background-color: #DC34E0;
+    width: 100%;
+    height: 4px;
+    background-color: #DC34E0; /* 三本線の色を指定 */
+    transition: transform 0.3s, opacity 0.3s;
+  }
+  #menu-toggle-checkbox {
+    display: none; /* チェックボックスを非表示にする */
+  }
+  #menu-toggle-checkbox:checked + .menu-toggle-button span:nth-child(1) {
+    transform: rotate(45deg) translateY(10px);
+  }
+  #menu-toggle-checkbox:checked + .menu-toggle-button span:nth-child(2) {
+    opacity: 0;
+  }
+  #menu-toggle-checkbox:checked + .menu-toggle-button span:nth-child(3) {
+    transform: rotate(-45deg) translateY(-10px);
   }
   .menu {
     display: none;
+    position: absolute;
+    top: 100%; /* ヘッダーの下に表示 */
+    right: 0;
+    background-color: white;
+    border: 1px solid #ddd;
+    box-shadow: 0 2px 10px rgba(0,0,0,0.1);
+    z-index: 999; /* メニューを前面に表示 */
+    width: 200px; /* メニューの幅を指定 */
+  }
+  .menu li {
+    list-style: none;
+  }
+  .menu li a {
+    display: block;
+    padding: 10px 20px;
+    color: black;
+    text-decoration: none;
+  }
+  .menu li a:hover {
+    background-color: #f0f0f0;
   }
   #menu-toggle-checkbox:checked + .menu-toggle-button + .menu {
     display: block;
   }
-  .icon-container {
-    position: relative;
-    left: -100px; /* 左に100px移動 */
+  .search-container {
+    display: flex;
+    justify-content: center; /* 中央揃え */
+    align-items: center; /* 縦方向の中央揃え */
+    flex-grow: 1; /* 余白を全て埋める */
   }
   .search-container input[type="text"] {
     height: 40px;
@@ -66,6 +106,35 @@
     font-size: 16px; /* フォントサイズを指定 */
     border: 2px solid #DC34E0; /* 枠線の色と太さを指定 */
     color: #DC34E0; /* 文字の色を指定 */
+  }
+  @media screen and (max-width: 30px) {
+    .Header {
+      flex-wrap: wrap;
+      justify-content: center;
+      height: auto;
+    }
+    .logo {
+      order: 1;
+      width: 100%;
+      text-align: center;
+      margin-bottom: 10px;
+    }
+    .search-container {
+      order: 2;
+      width: 100%;
+      margin-bottom: 10px;
+    }
+    .icon-container {
+      order: 3;
+      justify-content: center;
+      width: 100%;
+      margin-bottom: 10px;
+    }
+    .menu-toggle {
+      order: 4;
+      width: 100%;
+      text-align: center;
+    }
   }
 </style>
 </head>
@@ -83,24 +152,16 @@
     <p class="user-name">my_page</p>
   </div>
 
-  <!-- アイコンに枠線を追加 -->
-  <div class="icon-container">
-    <div class="icon icon1">
-      <a href="login.php" target="_self">
-        <img src="../images/normal_icon.png" alt="ログイン" border="0">
-      </a>
-    </div>
-
     <!-- 位置情報に枠線を追加 -->
-    <div class="icon icon2">
+    <div class="icon icon1">
       <a href="favorite.php" target="_self">
         <img src="../images/location_information.png" alt="お気に入り" border="0">
       </a>
     </div>
 
     <!-- アップロードを追加 -->
-    <div class="icon icon3">
-      <a href="photo_upp_button.php" target="_self">
+    <div class="icon icon2">
+      <a href="../G5-1/input.php" target="_self">
         <img src="../images/photo_upp_button.png" alt="カート" border="0">
       </a>
     </div>
@@ -114,12 +175,10 @@
       <span></span>
     </label>
     <ul class="menu">
-      <li><a href="#">Home</a></li>
-      <li><a href="#">Post</a></li>
-      <li><a href="#">Toukou</a></li>
-      <li><a href="#">My Page</a></li>
+      <li><a href="../G2-1/G2-1.php">Home</a></li>
+      <li><a href="../G5-1/input.php">Post</a></li>
       <li><a href="#">Option</a></li>
-      <li><a href="#">Logout</a></li>
+      <li><a href="../G1-4-1/G1-4-1-input.php">Logout</a></li>
     </ul>
   </div>
 </div>

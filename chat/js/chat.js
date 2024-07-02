e51925a4dc90c0c0c9d32ef87f9bdbc78e1db7cc
Rabbit0828 <kurihara0828syou@gmail.com>
2024-07-03 05:39:06 +0900

    s

--- chat/js/chat.js
+++ chat/js/chat.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     // メッセージ送信
     $('#bms_send_btn').click(function() {
-        var message = $('#bms_send_message').val().trim();
-        if (message !== '') {
+        var message = $('#bms_send_message').val();
+        if (message.trim() !== '') {
             $.ajax({
                 url: 'send_message.php',
                 type: 'POST',
@@ -10,15 +10,21 @@ $(document).ready(function() {
                 success: function(response) {
                     var res = JSON.parse(response);
                     if (res.status === 'success') {
-                        appendMessage('あなた', message, 'bms_right');
+                        $('#bms_messages').append(
+                            '<div class="bms_message bms_right">' +
+                            '<div class="bms_message_box">' +
+                            '<div class="bms_message_content">' +
+                            '<div class="bms_message_text">' + message + '</div>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div><div class="bms_clear"></div>'
+                        );
                         $('#bms_send_message').val('');
-                        scrollToBottom();
-                    } else {
-                        alert('メッセージの送信に失敗しました。');
+                        $('#bms_messages').scrollTop($('#bms_messages')[0].scrollHeight); // 自動スクロール
                     }
                 },
                 error: function() {
-                    alert('メッセージの送信に失敗しました。');
+                    // エラー時の処理を空にすることでアラートが表示されなくなります
                 }
             });
         }
@@ -34,36 +40,25 @@ $(document).ready(function() {
                 $('#bms_messages').html('');
                 messages.forEach(function(msg) {
                     var alignment = (msg.user === 'あなた') ? 'bms_right' : 'bms_left';
-                    appendMessage(msg.user, msg.message, alignment);
+                    $('#bms_messages').append(
+                        '<div class="bms_message ' + alignment + '">' +
+                        '<div class="bms_message_box">' +
+                        '<div class="bms_message_content">' +
+                        '<div class="bms_message_text">' + msg.message + '</div>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div><div class="bms_clear"></div>'
+                    );
                 });
-                scrollToBottom();
+                $('#bms_messages').scrollTop($('#bms_messages')[0].scrollHeight); // 自動スクロール
             },
             error: function() {
-                alert('メッセージの取得に失敗しました。');
+                // エラー時の処理を空にすることでアラートが表示されなくなります
             }
         });
     }
 
     // 定期的にメッセージを取得
     setInterval(fetchMessages, 3000); // 3秒ごとにメッセージを取得
-
-    // 新しいメッセージを表示する
-    function appendMessage(user, message, alignment) {
-        var escapedMessage = $('<div>').text(message).html(); // XSS対策のためにエスケープ処理
-        $('#bms_messages').append(
-            '<div class="bms_message ' + alignment + '">' +
-            '<div class="bms_message_box">' +
-            '<div class="bms_message_content">' +
-            '<div class="bms_message_text">' + escapedMessage + '</div>' +
-            '</div>' +
-            '</div>' +
-            '</div><div class="bms_clear"></div>'
-        );
-    }
-
-    // メッセージ表示領域を一番下までスクロールする
-    function scrollToBottom() {
-        $('#bms_messages').animate({ scrollTop: $('#bms_messages')[0].scrollHeight }, 'fast');
-    }
 });
 

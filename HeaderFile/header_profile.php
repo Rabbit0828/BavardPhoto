8aaf86e15700277cc64cbb80a3a42cac8e474da7
2201236 <2201236@s.asojuku.ac.jp>
2024-07-18 11:30:00 +0900

    aa

--- HeaderFile/header_profile.php
+++ HeaderFile/header_profile.php
@@ -173,7 +173,13 @@ $icon_path = '../images/' . $_SESSION['UserTable']['icon'];
     <!-- アップロードを追加 -->
     <div class="icon icon2">
       <a href="../G5-1/input.php" target="_self">
-        <img src="../images/photo_upp_button.png" alt="カート" style="height: 70px;width:75px;" border="0">
+        <img src="../images/photo_upp_button.png" alt="アップ" style="height: 70px;width:75px;" border="0">
+      </a>
+    </div>
+
+    <div class="icon icon3">            <!--修正作業よろしく-->
+      <a href="../chat/chat.php?user_id=<?php echo $user_id;?>" target="_self">
+        <img src="../images/chat.png" alt="チャット" style="height: 70px;width:75px;" border="0">
       </a>
     </div>
   </div>
